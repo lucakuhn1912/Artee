e6085f4fd50b89a9ea428cae2af352832847cf67
Tarek Rahme <rahmetarek@gmail.com>
2019-05-16 21:30:34 +0100

    Arts controller and views. View of new v1.0

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -1,5 +1,5 @@
 class ArtsController < ApplicationController
-  before_action :set_art, only: [:show, :edit, :update, :destroy]
+  before_action :set_art, only: %i[show edit update destroy]
 
   def index
     @arts = Art.all
@@ -14,7 +14,14 @@ class ArtsController < ApplicationController
   end
 
   def create
+    @owner = User.create(name: "TeeAr")
     @art = Art.new(art_params)
+    @art.owner = current_user
+    if @art.save
+      redirect_to arts_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -23,20 +30,26 @@ class ArtsController < ApplicationController
 
   def update
     # set_art
+    if @art.update(art_params)
+      redirect_to art_path(@art)
+    else
+      render :edit
+    end
   end
 
   def destroy
     # set_art
+    @art.destroy
+    redirect_to arts_path
   end
 
   private
 
   def art_params
-    params.require(:art).permit(:name, :description,
-     :artist, :location, :price, :value, :availability, :picture)
+    params.require(:art).permit(:name, :description, :artist,
+                                :location, :price, :value, :availability,
+                                :picture)
   end
-
-  # for owner use current_user, do picture with cloudinary
 
   def set_art
     @art = Art.find(params[:id])
