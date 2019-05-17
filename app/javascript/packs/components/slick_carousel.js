0f84a1d54c7e8c7178f668d378c2cb8103a98427
Luca Kuhn <luca.kuhn@hotmail.de>
2019-05-17 13:12:08 +0100

    homepage with carousel

---
+++ app/javascript/packs/components/slick_carousel.js
@@ -0,0 +1,30 @@
+import 'slick-carousel';
+
+$('.slick-carousel').slick({
+  prevArrow: '<i class="fas fa-chevron-left"></i>',
+  nextArrow: '<i class="fas fa-chevron-right"></i>',
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 2,
+  autoplay: true,
+  responsive: [
+  {
+    breakpoint: 992,
+    settings: {
+      slidesToShow: 2
+    }
+  },
+  {
+    breakpoint: 1200,
+    settings: {
+      slidesToShow: 3
+    }
+  },
+  {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 1
+    }
+  }
+  ]
+});
