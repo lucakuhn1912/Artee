8e9d2751a55506e1d79143ba4fcb0e29c3fb0396
Luca Kuhn <luca.kuhn@hotmail.de>
2019-05-20 14:39:57 +0100

    geocoding and index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_154715) do
+ActiveRecord::Schema.define(version: 2019_05_20_111720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2019_05_16_154715) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["owner_id"], name: "index_arts_on_owner_id"
   end
 
